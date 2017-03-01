e29774286e2cd248ab68530308cdb10c8dfdab72
AChapNoander <alexnoander14@gmail.com>
2017-03-01 02:30:30 -0500

    First Pass at Server Client Backend. Run Server and game_key, will open pygame window which registers key presses and sends them to server.

---
+++ com_server.py
@@ -0,0 +1,77 @@
+#!/usr/bin python3
+""" A simple chat TCP server """
+import socket
+import select
+import model_object
+
+test = model_object.model()
+
+
+def broadcast_data(message):
+    """ Sends a message to all sockets in the connection list. """
+    # Send message to everyone, except the server.
+    for sock in CONNECTION_LIST:
+        if sock != SERVER_SOCKET:
+            try:
+                sock.sendall(message)  # send all data at once
+            except Exception as msg:  # Connection was closed. Errors
+                print(type(msg).__name__)
+                sock.close()
+                try:
+                    CONNECTION_LIST.remove(sock)
+                except ValueError as msg:
+                    print("{}:{}".format(type(msg).__name__, msg))
+
+
+CONNECTION_LIST = []
+RECV_BUFFER = 4096  # Advisable to keep it as an exponent of 2
+PORT = 1337
+
+SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER_SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+SERVER_SOCKET.bind(("10.7.64.90", PORT))  # empty addr string means INADDR_ANY
+
+print("Listening...")
+SERVER_SOCKET.listen(2)  # 10 connections
+
+CONNECTION_LIST.append(SERVER_SOCKET)
+print("Server started!")
+
+while True:
+    # Get the list sockets which are ready to be read through select
+    READ_SOCKETS, WRITE_SOCKETS, ERROR_SOCKETS = select.select(CONNECTION_LIST, [], [])
+    for SOCK in READ_SOCKETS:  # New connection
+        # Handle the case in which there is a new connection recieved through server_socket
+        if SOCK == SERVER_SOCKET:
+            SOCKFD, ADDR = SERVER_SOCKET.accept()
+            CONNECTION_LIST.append(SOCKFD)  # add socket descriptor
+            # Adding \r to prevent message overlapping when another user
+            # types it's message.
+            print("\rClient ({0}, {1}) connected".format(ADDR[0], ADDR[1]))
+            broadcast_data("Client ({0}:{1}) entered room\n"
+                           .format(ADDR[0], ADDR[1]).encode())
+        else:  # Some incoming message from a client
+            try:  # Data recieved from client, process it
+                DATA = SOCK.recv(RECV_BUFFER)
+                if DATA:
+                    ADDR = SOCK.getpeername()  # get remote address of the socket
+                    message = "\r[{}:{}]: {}".format(ADDR[0], ADDR[1], DATA.decode())
+
+                    print(message)
+                    test.update_socket_list(ADDR[1])
+                    test.save_val(DATA.decode(), ADDR[1])
+
+                    broadcast_data(message.encode())
+            except Exception as msg:  # Errors happened, client disconnected
+                print(type(msg).__name__, msg)
+                print("\rClient ({0}, {1}) disconnected.".format(ADDR[0], ADDR[1]))
+                broadcast_data("\rClient ({0}, {1}) is offline\n"
+                               .format(ADDR[0], ADDR[1]).encode())
+                SOCK.close()
+                try:
+                    CONNECTION_LIST.remove(SOCK)
+                except ValueError as msg:
+                    print("{}:{}.".format(type(msg).__name__, msg))
+    test.update_physics()
+
+SERVER_SOCKET.close()
---
+++ model_object.py
@@ -0,0 +1,139 @@
+import time
+import ast
+max_ACC = 1
+
+
+class model(object):
+    def __init__(self):
+        self.state = 1
+        self.x = 0
+        self.y = 0
+        self.socket_list = []
+        self.users = []
+        self.correlation = {}
+
+    def save_val(self, thruput, member):
+        print(thruput)
+        to_use = self.correlation[member]
+        try:
+            ls = ast.literal_eval(thruput)
+            to_use.input(ls)
+        except (SyntaxError, ValueError) as e:
+            pass
+
+    def update_socket_list(self, thruput):
+        if thruput not in self.socket_list:
+            self.socket_list.append(thruput)
+            if len(self.socket_list) == 1:
+                self.users.append(ship([1, 1, .5, .5, .25, .25, 0, -1]))
+                self.correlation[thruput] = self.users[0]
+
+            elif len(self.socket_list) == 2:
+                self.users.append(gunner([0, 0, 0, 0, 0, 0, 0, 0]))
+                self.correlation[thruput] = self.users[1]
+
+            else:
+                print('Invalid number of connections: 2 players expected.')
+                print('Ignoring Extra Connection')
+
+    def update_physics(self):
+        # print(time.time())
+        for i in self.users:
+            i.update_physics()
+        # print('updated')
+        # print(self.users)
+        # print(self.correlation)
+
+
+class Point:
+    """ Point class represents and manipulates x,y coords. """
+
+    def __init__(self, x=0, y=0):
+        """ Create a new point at x, y """
+        self.x = x
+        self.y = y
+
+    def multiply_by(self, coefficient):
+        return Point(self.x * coefficient, self.y * coefficient)
+
+    def add(self, other_point):
+        return Point(self.x + other_point.x, self.y + other_point.y)
+
+    def __str__(self):
+        return '{} {}'.format(self.x, self.y)
+
+
+class base_class(object):
+    """
+    Base Class to make life easier for everyone involved
+    creates base methods accessible to all objects that inherit it, i.e. ship
+        and gunner.
+    """
+    def __init__(self, vector=[0, 0, 0, 0, 0, 0, 0, 0]):
+        # [x,y] [dx,dy] [ddx,ddy] [theta,dtheta]
+        self.loc = Point(vector[0], vector[1])
+        self.vel = Point(vector[2], vector[3])
+        self.acc = Point(vector[4], vector[5])
+        self.rot = Point(vector[6], vector[7])
+        self.last_time = time.time()
+
+    def update_physics(self):
+        current = time.time()
+        diff = current - self.last_time
+        self.last_time = current
+
+        # calculates change in POSITION by eulers method
+        delta_vel = self.acc.multiply_by(diff)
+        self.vel = self.vel.add(delta_vel)
+        delta_pos = self.vel.multiply_by(diff)
+        self.loc = self.loc.add(delta_pos)
+        # self.acc = Point(0, 0)  # resets acceleration point afterwards
+
+        # calculates change in ROTATION by eulers method
+        delta_theta = self.rot.y * diff
+        self.rot.x += delta_theta
+        # self.rot.y = self.rot.y / 2  # trailing rotational motion
+        # print(self.loc, self.vel, self.acc, self.rot)
+        print(self.acc)
+
+
+class gunner(base_class):
+    """
+        gunner class has little to no physics run on it, as it is always
+        positioned in the middle of the ship. The only possible physics is
+        in the rotation of the turret, encapsulated by the self.acc point.
+    """
+    def __str__(self):
+        return "I'm a gunner"
+
+    def input(self, ls):
+        print('gunner')
+        # controls: left, right, up, down, space
+        # equivalent:0, 1, 2, 3, 4
+        for val in ls:
+            if val == 0:
+                self.acc.x = -max_ACC
+            elif val == 1:
+                self.acc.x = -max_ACC
+            elif val == 4:
+                print('bang bang')
+
+
+class ship(base_class):
+    def __str__(self):
+        return "I'm a ship"
+
+    def input(self, ls):
+        # controls: left, right, up, down, space
+        # equivalent:0, 1, 2, 3, 4
+        for val in ls:
+            if val == 0:
+                self.acc.x = -max_ACC
+            elif val == 1:
+                self.acc.x = max_ACC
+            elif val == 2:
+                self.acc.x = -max_ACC
+            elif val == 3:
+                self.acc.x = -max_ACC
+            elif val == 4:
+                print('bang bang')
