1ffc1cfe4a215bf40d514ab1fba5b5f1bba161fc
segerpeter07 <segerpeter07@gmail.com>
2017-03-05 14:37:00 -0500

    Cleaned up

--- tictactoe.py
+++ tictactoe.py
@@ -10,36 +10,6 @@ import pygame
 import time
 
 
-class board():
-    """
-    This is the game board which contains functions to:
-    -Create the board
-    -Update the board
-    -Print the board
-    -Add a play piece
-    -Clear the board
-    """
-
-    def __init__(self):
-        self.board_vals = {1: ' ', 2: ' ', 3: ' ', 4: ' ', 5: ' ', 6: ' ', 7: ' ', 8: ' ', 9: ' '}
-
-    def print_board(self):
-        print('   |   |')
-        print(' ' + self.board_vals[1] + ' | ' + self.board_vals[2] + ' | ' + self.board_vals[3])
-        print('   |   |')
-        print('-----------')
-        print('   |   |')
-        print(' ' + self.board_vals[4] + ' | ' + self.board_vals[5] + ' | ' + self.board_vals[6])
-        print('   |   |')
-        print('-----------')
-        print('   |   |')
-        print(' ' + self.board_vals[7] + ' | ' + self.board_vals[8] + ' | ' + self.board_vals[9])
-        print('   |   |')
-
-    def insert_play(self, play, play_piece):
-        self.board_vals[play] = play_piece
-
-
 class Board():
     def __init__(self, height=300, width=300):
         self.height = height
@@ -109,20 +79,7 @@ class PyGameWindowView:
         pygame.display.update()
 
 
-# class PyGameMouseController:
-#     def __init__(self, model):
-#         self.model = model
-#
-#     def handle_mouse_event(self, event):
-#         if event.type == pygame.MOUSEMOTION:
-#             self.model.piece.x = event.pos[0] - self.model.piece.width/2.0
-
-
 if __name__ == '__main__':
-    # test = board()
-    # test.insert_play(3, "X")
-    # test.print_board()
-
     pygame.init()
 
     size = (840, 840)
