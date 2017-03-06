27e4f13fc01cb50ef5d4e039775e28f6f8529486
AChapNoander <alexnoander14@gmail.com>
2017-03-06 15:42:09 -0500

    Commenting and Documenting

--- com_client.py
+++ com_client.py
@@ -1,13 +1,25 @@
-#!/usr/bin python3
-""" TCP Client """
-
+"""
+    Client Object for Super Tic Tac tic_tac_toe
+    @Author Alex Chapman
+    3/6/17
+"""
 import socket
 import select
 import sys
 
 
 class client(object):
+    """
+        Client class encapsulating TCP client-side code
+
+        Attributes:
+            HOST            - IP address of server
+            PORT            - Port number of hosted server
+            MASTER_SOCK     - Master connection to server
+            READ_SOCKETS    - list of sockets to read messages from
+    """
     def __init__(self, HOST='10.7.8.33', PORT=1337):
+        """Returns Client Object"""
         self.HOST = HOST
         self.PORT = PORT
 
@@ -26,11 +38,13 @@ class client(object):
             sys.exit()
 
     def send_message(self, message):
+        """Sends message to the server"""
         if message is not 'hb':
             print(message)
         self.MASTER_SOCK.sendall(message.encode())
 
     def check_messages(self):
+        """Checks read sockets for any messages and returns them"""
         for sock in self.READ_SOCKETS:  # incoming message from remote server
             if sock == self.MASTER_SOCK:
                 data = sock.recv(4096)
@@ -43,6 +57,7 @@ class client(object):
                     return data.decode()
 
     def messages(self, msg):
+        """ Idea for lightweighting implementation. Unimplemented."""
         for sock in self.READ_SOCKETS:  # incoming message from remote server
             if sock == self.MASTER_SOCK:
                 data = sock.recv(4096)
